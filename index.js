c40673dac81c4e3bb7ecddae8cfd580a74cc1e58
MUHAMMAD NABEEL NISAR <muhammad.nabeel.nisar@gmail.com>
2022-09-21 07:38:43 -0700

    init

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const app = express();
-const product = require("./api/product");
+const express = require("express")
+const app = express()
+const product = require("./api/product")
+const main = require("./api/main")
 
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }))
 
-app.use("/api/product", product);
+app.use("/product", product)
+app.use("/", main)
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
